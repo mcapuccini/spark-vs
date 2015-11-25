6a8d6a589744993ec5994db5ef0fd3ad07ace48d
laeeq80 <laeeq@kth.se>
2015-11-25 19:39:28 +0100

    parseScore does not like the coming data

--- vs/src/main/scala/se/uu/farmbio/vs/ConformerPipeline.scala
+++ vs/src/main/scala/se/uu/farmbio/vs/ConformerPipeline.scala
@@ -22,15 +22,31 @@ class ConformerPipeline[vs](override val rdd: RDD[String])
 //    new PosePipeline(res)
     val cppExePath = "/home/laeeq/Desktop/spark-vs/docking-cpp/dockingstd"
     val pipedRDD = rdd.pipe(cppExePath)
+    
+    val res = pipedRDD.flatMap(SBVSPipeline.splitSDFmolecules)
+    
+    
+    /*
     val res = pipedRDD.collect()
+       
+     
     val string = res.mkString("\n")
     val res2 = SBVSPipeline.splitSDFmolecules(string)
     val cppRDD = sc.makeRDD(res2)    
-    new PosePipeline(cppRDD)
+    */
     
-      //throw new NotImplementedException("Needs to be reimplemented due to memory issue")
+    new PosePipeline(res)
+    
+    
+    
+    
+    //throw new NotImplementedException("Needs to be reimplemented due to memory issue")
   }
 
+  
+  
+  
+  
   override def repartition() = {
     val res = rdd.repartition(defaultParallelism)
     new ConformerPipeline(res)
