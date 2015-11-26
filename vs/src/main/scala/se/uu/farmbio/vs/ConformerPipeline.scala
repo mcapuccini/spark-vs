ffd2a479b61d8da99013190217cafc0786c58b3b
Valentin Georgiev <valentin.georgiev@farmbio.uu.se>
2015-11-26 10:05:04 +0100

    Add generateSignatures method to ConformerPipeline

--- vs/src/main/scala/se/uu/farmbio/vs/ConformerPipeline.scala
+++ vs/src/main/scala/se/uu/farmbio/vs/ConformerPipeline.scala
@@ -4,11 +4,15 @@ import java.io.InputStream
 
 import org.apache.commons.lang.NotImplementedException
 import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.regression.LabeledPoint
+import uu.farmbio.sg.SGUtils
+
 
 trait ConformerTransforms {
 
   def dock(receptor: InputStream, method: Int, resolution: Int): SBVSPipeline with PoseTransforms
   def repartition: SBVSPipeline with ConformerTransforms
+  def generateSignatures() : ICPMLTransforms
 
 }
 
@@ -28,4 +32,14 @@ class ConformerPipeline[vs](override val rdd: RDD[String])
     new ConformerPipeline(res)
   }
 
+  def generateSignatures = {
+    val molsCount = rdd.count()
+    val molsWithIndex = rdd.zipWithIndex()
+    val molsAfterSG = molsWithIndex.flatMap{case(mol, index) => Sdf2LibSVM.sdf2signatures(mol,index + 1,molsCount)} //Compute signatures
+    .cache
+    val (result, sig2ID_universe) = SGUtils.sig2ID_carryData(molsAfterSG)
+    val resultAsLP : RDD[(Long, LabeledPoint)] = SGUtils.sig2LP_carryData(result, sc);
+
+    new ICPMLPipeline(resultAsLP)
+ }
 }
