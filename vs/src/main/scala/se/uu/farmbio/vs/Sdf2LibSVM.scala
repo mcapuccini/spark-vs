8ed87143b35bccdb1c786192afd815a5ddfe8270
Valentin Georgiev <valentin.georgiev@farmbio.uu.se>
2015-11-26 10:03:56 +0100

    Add Sdf2LibSVM.scala

---
+++ vs/src/main/scala/se/uu/farmbio/vs/Sdf2LibSVM.scala
@@ -0,0 +1,48 @@
+package se.uu.farmbio.vs
+
+import org.openscience.cdk.io.MDLV2000Reader
+import java.nio.charset.Charset
+import org.openscience.cdk.tools.manipulator.ChemFileManipulator
+import java.io.ByteArrayInputStream
+import org.openscience.cdk.silent.ChemFile
+import uu.farmbio.sg.SGUtils
+import uu.farmbio.sg.types.SignatureRecordDecision
+
+
+/**
+ * @author laeeq
+ */
+private[vs] object Sdf2LibSVM {
+  
+  def sdf2signatures = (poses: String, index: Long, molCount: Long) => {
+    //get SDF as input stream
+    val sdfByteArray = poses
+      .getBytes(Charset.forName("UTF-8"))
+    val sdfIS = new ByteArrayInputStream(sdfByteArray)
+    //Parse SDF
+    val reader = new MDLV2000Reader(sdfIS)
+    val chemFile = reader.read(new ChemFile)
+    val mols = ChemFileManipulator.getAllAtomContainers(chemFile)
+    //mols is a Java list :-(
+   
+    val it = mols.iterator
+    
+    var res = Seq[(Long,SignatureRecordDecision)]()
+    
+    while (it.hasNext()) {
+      //for each molecule in the record compute the signature
+      
+        val mol = it.next
+        val label = index.toDouble match { //convert labels
+        case x if x <= (molCount/2) => 1.0
+        case _ => 0.0
+      }
+      
+      val sig = (index, SGUtils.atom2SigRecordDecision(mol, label.toDouble, 1, 3)) // Using new method from Staffan new library
+      res = res ++ Seq(sig) 
+      
+    }
+    res //return the results
+  }
+  
+}
