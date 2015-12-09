3ef9018eadf68d07bf6596c0b657effa1717c027
mcapuccini <m.capuccini@gmail.com>
2015-12-09 16:44:35 +0100

    1) dock API fixed 2) get files is now inside the mapper Docking test still fails when results are compared to the old results

--- vs/src/main/scala/se/uu/farmbio/vs/ConformerPipeline.scala
+++ vs/src/main/scala/se/uu/farmbio/vs/ConformerPipeline.scala
@@ -10,47 +10,57 @@ import org.apache.spark.SparkFiles
 import org.apache.spark.rdd.RDD
 
 trait ConformerTransforms {
-  def dock(cppExePath: String, method: Int, resolution: Int, receptor: String): SBVSPipeline with PoseTransforms
+  def dock(receptorPath: String, method: Int, resolution: Int): SBVSPipeline with PoseTransforms
   def repartition: SBVSPipeline with ConformerTransforms
 }
 
-private[vs] class ConformerPipeline(override val rdd: RDD[String])
-    extends SBVSPipeline(rdd) with ConformerTransforms {
+object ConformerPipeline {
 
   //The Spark built-in pipe splits molecules line by line, we need a custom one
-  def pipe(command: List[String]) = {
+  private def pipeString(str: String, command: List[String]) = {
 
-    val res = rdd.map { sdf =>
-      //Start executable
-      val pb = new ProcessBuilder(command.asJava)
-      val proc = pb.start
-      // Start a thread to print the process's stderr to ours
-      new Thread("stderr reader") {
-        override def run() {
-          for (line <- Source.fromInputStream(proc.getErrorStream).getLines) {
-            System.err.println(line)
-          }
+    //Start executable
+    val pb = new ProcessBuilder(command.asJava)
+    val proc = pb.start
+    // Start a thread to print the process's stderr to ours
+    new Thread("stderr reader") {
+      override def run() {
+        for (line <- Source.fromInputStream(proc.getErrorStream).getLines) {
+          System.err.println(line)
         }
-      }.start
-      // Start a thread to feed the process input 
-      new Thread("stdin writer") {
-        override def run() {
-          val out = new PrintWriter(proc.getOutputStream)
-          out.println(sdf)
-          out.close()
-        }
-      }.start
-      //Return results as a single string
-      Source.fromInputStream(proc.getInputStream).mkString
-    }
-    new ConformerPipeline(res)
+      }
+    }.start
+    // Start a thread to feed the process input 
+    new Thread("stdin writer") {
+      override def run() {
+        val out = new PrintWriter(proc.getOutputStream)
+        out.println(str)
+        out.close()
+      }
+    }.start
+    //Return results as a single string
+    Source.fromInputStream(proc.getInputStream).mkString
+
   }
 
-  override def dock(cppExePath: String, method: Int, resolution: Int, receptor: String) = {
-    sc.addFile(cppExePath)
-    sc.addFile(receptor)
-    val p = Paths.get(receptor)
-    val pipedRDD = this.pipe(List(SparkFiles.get("dockingstd"), method.toString(), resolution.toString(), SparkFiles.get(p.getFileName().toString()))).getMolecules
+}
+
+private[vs] class ConformerPipeline(override val rdd: RDD[String])
+  extends SBVSPipeline(rdd) with ConformerTransforms {
+
+  override def dock(receptorPath: String, method: Int, resolution: Int) = {
+    val dockingstdPath = System.getenv("DOCKINGSTD")
+    sc.addFile(dockingstdPath)
+    sc.addFile(receptorPath)
+    val receptorFileName = Paths.get(receptorPath).getFileName.toString
+    val dockerstdFileName = Paths.get(dockingstdPath).getFileName.toString
+    val pipedRDD = rdd.map { sdf =>
+      ConformerPipeline.pipeString(sdf,
+        List(SparkFiles.get(dockerstdFileName), 
+          method.toString(),
+          resolution.toString(),
+          SparkFiles.get(receptorFileName)))
+    }
     val res = pipedRDD.flatMap(SBVSPipeline.splitSDFmolecules)
     new PosePipeline(res)
   }
--- vs/src/test/scala/se/uu/farmbio/vs/SBVSPipelineTest.scala
+++ vs/src/test/scala/se/uu/farmbio/vs/SBVSPipelineTest.scala
@@ -14,18 +14,18 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class SBVSPipelineTest extends FunSuite with BeforeAndAfterAll {
-  
+
   //Init Spark
   private val conf = new SparkConf()
     .setMaster("local")
     .setAppName("SBVSPipelineTest")
     .setExecutorEnv("OE_LICENSE", System.getenv("OE_LICENSE"))
   private val sc = new SparkContext(conf)
-  sc.hadoopConfiguration.set(SDFRecordReader.SIZE_PROPERTY_NAME, "3")  
+  sc.hadoopConfiguration.set(SDFRecordReader.SIZE_PROPERTY_NAME, "3")
   sc.hadoopConfiguration.set(SmilesRecordReader.SIZE_PROPERTY_NAME, "3")
 
   test("sortByScore should sort a set of poses by score") {
-    
+
     val res = new SBVSPipeline(sc)
       .readPoseFile(getClass.getResource("filtered_collapsed.sdf").getPath)
       .sortByScore
@@ -33,116 +33,101 @@ class SBVSPipelineTest extends FunSuite with BeforeAndAfterAll {
       .collect
 
     val sortedPoses = TestUtils.readSDF(getClass.getResource("filtered_collapsed_sorted.sdf").getPath)
-    assert(res === sortedPoses)  
-      
+    assert(res === sortedPoses)
+
   }
-  
+
   test("collapse should collapse poses with same id to n with highest score") {
-    
+
     val n = 2
-    
+
     val res = new SBVSPipeline(sc)
       .readPoseFile(getClass.getResource("filtered_poses.sdf").getPath)
       .collapse(n)
       .getMolecules
       .collect
-   
+
     val filteredCollapsed = TestUtils.readSDF(getClass.getResource("filtered_collapsed.sdf").getPath)
-    assert(res.toSet === filteredCollapsed.toSet)    
-      
+    assert(res.toSet === filteredCollapsed.toSet)
+
   }
-  
+
   test("filter should filter a set of SMILES according to the provided custom filter") {
-    
-   val res = new SBVSPipeline(sc)
+
+    val res = new SBVSPipeline(sc)
       .readSmilesFile(getClass.getResource("molecules.smi").getPath)
       .filter(getClass.getResourceAsStream("zinc_filter.txt"))
       .getMolecules
       .collect
-      
+
     val filtered = TestUtils.readSmiles(getClass.getResource("custom_filtered.smi").getPath)
-    assert(res.flatMap(TestUtils.splitSmiles).toSet === filtered.toSet)  
-      
+    assert(res.flatMap(TestUtils.splitSmiles).toSet === filtered.toSet)
+
   }
-  
+
   test("filter should filter a set of SMILES according to the provided filter type") {
-    
+
     val res = new SBVSPipeline(sc)
       .readSmilesFile(getClass.getResource("molecules.smi").getPath)
       .filter(OEFilterType.Lead)
       .getMolecules
       .collect
-      
+
     val filtered = TestUtils.readSmiles(getClass.getResource("filtered.smi").getPath)
-    assert(res.flatMap(TestUtils.splitSmiles).toSet === filtered.toSet)  
-      
+    assert(res.flatMap(TestUtils.splitSmiles).toSet === filtered.toSet)
+
   }
-  
+
   test("generateConformers should generate conformers from a SMILES file according to a maxCenters parameter") {
-    
+
     val res = new SBVSPipeline(sc)
       .readSmilesFile(getClass.getResource("filtered.smi").getPath)
       .generateConformers(12)
       .getMolecules
-      .collect  
-      
+      .collect
+
     val filteredConformers = TestUtils.readSDF(getClass.getResource("filtered_conformers.sdf").getPath)
-    
+
     val resSet = res.flatMap(TestUtils.splitSDF).map(TestUtils.removeSDFheader).toSet
     val filtTest = filteredConformers.map(TestUtils.removeSDFheader).toSet
-    assert(resSet === filtTest)  
-    
+    assert(resSet === filtTest)
+
   }
-  
-  test("generateConformers should generate conformers from a SMILES file according to maxCenters, maxConformers" 
-    +" parameters") {
-    
+
+  test("generateConformers should generate conformers from a SMILES file according to maxCenters, maxConformers"
+    + " parameters") {
+
     val res = new SBVSPipeline(sc)
       .readSmilesFile(getClass.getResource("filtered.smi").getPath)
-      .generateConformers(2,1)
+      .generateConformers(2, 1)
       .getMolecules
-      .collect  
-      
+      .collect
+
     val filteredConformers = TestUtils.readSDF(getClass.getResource("filtered_single.sdf").getPath)
-    
+
     val resSet = res.flatMap(TestUtils.splitSDF).map(TestUtils.removeSDFheader).toSet
     val filtTest = filteredConformers.map(TestUtils.removeSDFheader).toSet
-    assert(resSet === filtTest)  
-    
+    assert(resSet === filtTest)
+
   }
-  
-  test("pipe should execute an external program over some conformers") {
-    
-    val toPipe = new SBVSPipeline(sc)
-      .readConformerFile(getClass.getResource("filtered_conformers.sdf").getPath)
-      .asInstanceOf[ConformerPipeline]
-    
-    //Try to count lines with wc
-    val wc = toPipe.pipe(List("wc", "-l"))
-      .getMolecules
-      .map(_.trim.toInt)
-      .sum
-    assert(wc == 998)
-    
-  }
-  
+
   test("dock should dock a set of conformers to a receptor and generate the poses") {
 
     val res = new SBVSPipeline(sc)
       .readConformerFile(getClass.getResource("filtered_conformers.sdf").getPath)
-      .dock(System.getenv("DOCKING_CPP"), 
-          OEDockMethod.Chemgauss4, OESearchResolution.Standard,getClass.getResource("receptor.oeb").getPath)
+      .dock(getClass.getResource("receptor.oeb").getPath, 
+        OEDockMethod.Chemgauss4, OESearchResolution.Standard)
       .getMolecules
       .collect
-      
+
     val filteredPoses = TestUtils.readSDF(getClass.getResource("filtered_poses.sdf").getPath)
-    assert(res.map(TestUtils.removeSDFheader).toSet 
-        === filteredPoses.map(TestUtils.removeSDFheader).toSet)
-    
+    assert(res.map(TestUtils.removeSDFheader).toSet
+      === filteredPoses.map(TestUtils.removeSDFheader).toSet)
+
   }
-  
+
   override def afterAll() {
-    sc.stop() 
+    sc.stop()
   }
-  
+
 }
