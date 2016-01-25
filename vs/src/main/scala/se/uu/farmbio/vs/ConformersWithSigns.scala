7e5ca5fbaf96357c50f5014ae735583ae73e8580
laeeq80 <laeeq@kth.se>
2016-01-25 16:11:48 +0100

    updating working local to remote

--- vs/src/main/scala/se/uu/farmbio/vs/ConformersWithSigns.scala
+++ vs/src/main/scala/se/uu/farmbio/vs/ConformersWithSigns.scala
@@ -10,9 +10,15 @@ import org.apache.spark.SparkFiles
 import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.util.MLUtils
+import se.uu.farmbio.cp.AggregatedICPClassifier
+import se.uu.farmbio.cp.BinaryClassificationICPMetrics
+import se.uu.farmbio.cp.ICP
+import se.uu.farmbio.cp.ICPClassifierModel
+import se.uu.farmbio.cp.alg.GBT
 
 trait ConformersWithSignsTransforms {
   def saveAsSignatureFile(path: String)
+
 }
 
 private[vs] class ConformersWithSigns(val rdd: RDD[(Long, LabeledPoint)])
